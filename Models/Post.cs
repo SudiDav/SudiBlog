6be074487354bc85165fc39329f92873b22ad4c6
Sudi Simba <sudisimbadav@gmail.com>
2021-07-10 18:30:43 +0200

    Completed the Post model

---
+++ Models/Post.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SudiBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int BlogId { get; set; }
+        public string AutorId { get; set; }
+        [Required]
+        [StringLength(75, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters.", MinimumLength = 3)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(200, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters.", MinimumLength = 3)]
+        public string Abstract { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
+        public DateTime Created { get; set; }
+        [Display(Name = "Updated Date")]
+        [DataType(DataType.Date)]
+        public DateTime? Updated { get; set; }
+        public bool IsReady { get; set; }
+        public string Slug { get; set; }
+        public byte[] ImageData { get; set; }
+        [Display(Name = "Image Type")]
+        public string COntentType { get; set; }
+        [NotMapped]
+        public IFormFile Image { get; set; }
+    }
+}
