4ff81d810c40c4cb5f4ed827fc8ba6911bc66866
Sudi Simba <sudisimbadav@gmail.com>
2021-07-13 21:54:13 +0200

    Scafolded Identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(SudiBlog.Areas.Identity.IdentityHostingStartup))]
+namespace SudiBlog.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                //services.AddDbContext<ApplicationDbContext>(options =>
+                //    options.UseNpgsql(
+                //        context.Configuration.GetConnectionString("ApplicationDbContextConnection")));
+
+                //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                //    .AddEntityFrameworkStores<ApplicationDbContext>();
+            });
+        }
+    }
+}
