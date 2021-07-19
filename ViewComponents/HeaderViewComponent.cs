fd913a1b703e6239ef18109c6703bf5b9956fc65
Sudi Simba <sudisimbadav@gmail.com>
2021-07-19 13:00:37 +0200

    Completed ViewComponents of Nav, Header and footer

---
+++ ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SudiBlog.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.Factory.StartNew(() =>
+            {
+                return View();
+            });
+        }
+    }
+}
