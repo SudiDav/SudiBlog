63298d714397bc2e0aa60a7de4f156b5578981e0
Sudi Simba <sudisimbadav@gmail.com>
2021-07-22 21:51:39 +0200

    completed email confirmation and password recovery.

--- Areas/Identity/Pages/Account/Manage/ExternalLogins.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/ExternalLogins.cshtml.cs
@@ -1,22 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SudiBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SudiBlog.Areas.Identity.Pages.Account.Manage
 {
     public class ExternalLoginsModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<BlogUser> _userManager;
+        private readonly SignInManager<BlogUser> _signInManager;
 
         public ExternalLoginsModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager)
+            UserManager<BlogUser> userManager,
+            SignInManager<BlogUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
