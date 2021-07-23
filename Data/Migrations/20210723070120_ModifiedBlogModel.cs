1cf314ad3bf03111fc1115a3e2e93509620a1286
Sudi Simba <sudisimbadav@gmail.com>
2021-07-23 09:05:00 +0200

    modified migrations

---
+++ Data/Migrations/20210723070120_ModifiedBlogModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SudiBlog.Data.Migrations
+{
+    public partial class ModifiedBlogModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
