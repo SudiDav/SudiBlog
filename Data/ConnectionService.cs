8642fa1b655e93c19c2af3af2cf35783f0432065
Sudi Simba <sudisimbadav@gmail.com>
2021-11-15 16:43:42 +0200

    added namespace for connectionString class

---
+++ Data/ConnectionService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+
+namespace SudiBlog.Data
+{
+    public static class ConnectionService
+    {
+        public static string GetConectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        }
+        public static string BuildConnectionString(string databaseUrl)
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true
+
+            };
+
+            return builder.ToString();
+        }
+    }
+}
